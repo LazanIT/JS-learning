1785a96d6068a4a14676fa9aa7acbbff73c30d4d
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-05-19 13:30:48 +0200

    First commit
    My first JS script

---
+++ Predavanje 1/script.js
@@ -0,0 +1,6 @@
+var mojeIme = "Laza"; // Tip je string (Tacnije text) (Preporuceno je da se koriste dupli navodnici "")
+var godine = 20; // Int (Integer) je broj
+var visina = 181.4; // float je decimalni unos
+var programer = true; // Boolean tip i moze biti ( true / false ) ( 1 / 0)
+
+console.log(mojeIme, godine, visina, programer);
---
+++ Predavanje 1/vezba.js
@@ -0,0 +1,7 @@
+var ime = "LazaMaza";
+var prezime = "Lazanski";
+var godine = 20;
+var visina = 185.2;
+var programer = false;
+
+console.log(ime, prezime, godine, visina, programer);
