fec13885e25ab39b4af4e1d0e1d18f16f04bec4e
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-05-27 23:59:41 +0200

    First exercise
    Adding button and one border on click with JS

---
+++ Predavanje 10/script.js
@@ -0,0 +1,7 @@
+// console.log("test 123");
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function OnClick(event) {
+	console.log("test");
+	button.style.border = "4px double red";
+});
