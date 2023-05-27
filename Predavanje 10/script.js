ff14b26612d4b227bdb6ca0d6cd5f5d3aabf10a5
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-05-28 00:07:50 +0200

    First function commit
    Added first function and some comments with explain

--- Predavanje 10/script.js
+++ Predavanje 10/script.js
@@ -2,6 +2,14 @@
 const button = document.getElementById("btn");
 
 button.addEventListener("click", function OnClick(event) {
+	// I ovde mi pozivamo funkcije, ali su to anonimne funkcije
 	console.log("test");
 	button.style.border = "4px double red";
 });
+
+// Functions
+// Uloga funkcije je da grupise neku celinu koda koja treba da se izvrsi
+function sayHello() {
+	let name = prompt("Tell me your name");
+	alert(name);
+}
