db8876b630416cbad9e7d5d4a246afceded18ced
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-05-28 00:36:41 +0200

    Third exercise
    Combination function with eventlistener and math operation

--- Predavanje 10/script.js
+++ Predavanje 10/script.js
@@ -1,6 +1,7 @@
 // console.log("test 123");
 const button = document.getElementById("btn");
 const buttonAge = document.getElementById("btnAge");
+const buttonSplit = document.getElementById("splitBtn");
 
 button.addEventListener("click", function OnClick(event) {
 	// I ovde mi pozivamo funkcije, ali su to anonimne funkcije
@@ -9,21 +10,38 @@ button.addEventListener("click", function OnClick(event) {
 });
 buttonAge.addEventListener("click", age);
 
+buttonSplit.addEventListener("mouseover", split);
+
 // Functions
 // Uloga funkcije je da grupise neku celinu koda koja treba da se izvrsi
 // Samim tim izbegavamo ponavljanje tog koda
+
+// Prva vezba
 function sayHello() {
 	let name = prompt("Tell me your name");
 	alert(name);
 }
 
+// Druga vezba
 function age(yearOld) {
 	let birthday = prompt("tell me your age");
 	if (yearOld < 18) {
 		alert("You have under 18");
 	} else if (yearOld == 18) {
 		alert("You have 18");
-	} else {
+	} else if (yearOld > 18) {
 		alert("You have more than 18");
+	} else {
+		alert("You have not entered an age");
 	}
+}
+
+// Treca vezba
+function split(numberOne, numberTwo) {
+	numberOne = prompt("First number");
+	numberTwo = prompt("Second number");
+	if (numberTwo == 0) {
+		alert("Number can't be zero");
+	}
+	alert(numberOne / numberTwo);
 }
