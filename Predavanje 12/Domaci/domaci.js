50fa652d7eed8fc9d4ade56c90d92c825f400fa6
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-02 23:21:12 +0200

    Home work update 2
    Finished writing cities and showing without any condition

--- Predavanje 12/Domaci/domaci.js
+++ Predavanje 12/Domaci/domaci.js
@@ -1,3 +1,14 @@
 const cities = document.getElementById("cities");
 const addButton = document.getElementById("addCity");
 const showAllCities = document.getElementById("showAllCities");
+
+let objects = [];
+addButton.addEventListener("click", function click() {
+	let title = document.createElement("h4");
+	title.style.color = "purple";
+	title.style.fontSize = "3rem";
+	title.innerHTML = prompt("Add your city and temperature");
+	showAllCities.addEventListener("click", function click() {
+		cities.appendChild(title);
+	});
+});
