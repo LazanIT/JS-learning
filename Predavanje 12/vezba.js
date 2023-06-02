e94dd7f9121613bd26e7da26bd3792668d4c4e14
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-02 15:50:09 +0200

    Second exercise finished
    We have two type of working code, first is without array, second with array and its better in practice

--- Predavanje 12/vezba.js
+++ Predavanje 12/vezba.js
@@ -1,14 +1,31 @@
 const text = document.getElementById("text");
-let paragraph = document.createElement("h2");
-paragraph.innerHTML = prompt("What's your name");
-text.appendChild(paragraph);
-
+const city = document.getElementById("arr");
 const button = document.getElementById("btn");
-const buttonsShow = document.getElementById("btnShow");
+const buttonShow = document.getElementById("btnShow");
+const buttonAdd = document.getElementById("btnAdd");
+const buttonShowCity = document.getElementById("btnShowCity");
+// let paragraph = document.createElement("h2");
+// paragraph.innerHTML = prompt("What's your name");
+// text.appendChild(paragraph);
+let cities = [];
+
+// Prvi nacin za upis gradova i kasnije prikazivanje
 button.addEventListener("click", function click(event) {
 	let title = document.createElement("h2");
 	title.innerHTML = prompt("Add your city");
-	buttonsShow.addEventListener("mouseover", function mouseover() {
+	buttonShow.addEventListener("mouseover", function mouseover() {
 		text.appendChild(title);
+	});
+});
+// Drugi nacin za upis gradova i njihovo prikazivanje
+// Drugi nacin je preko array, tacnije napravimo array u koji cemo kasnije pushati elemente i na odredjeno dugme ih samo prikazivati
+buttonAdd.addEventListener("click", function click() {
+	let title = document.createElement("h2");
+	title.innerHTML = prompt("Add your city");
+	cities.push(title);
+	buttonShowCity.addEventListener("click", function click() {
+		for (let object in cities) {
+			city.appendChild(cities[object]);
+		}
 	});
 });
