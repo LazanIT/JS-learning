ac9b5138e80e14a3b550e6b9c84ae5f8d3538b33
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-02 15:08:02 +0200

    Second exercise part 2
    Added show button

--- Predavanje 12/vezba.js
+++ Predavanje 12/vezba.js
@@ -4,9 +4,11 @@ paragraph.innerHTML = prompt("What's your name");
 text.appendChild(paragraph);
 
 const button = document.getElementById("btn");
-
+const buttonsShow = document.getElementById("btnShow");
 button.addEventListener("click", function click(event) {
 	let title = document.createElement("h2");
 	title.innerHTML = prompt("Add your city");
-	text.appendChild(title);
+	buttonsShow.addEventListener("mouseover", function mouseover() {
+		text.appendChild(title);
+	});
 });
