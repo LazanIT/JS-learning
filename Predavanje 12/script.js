0fd2dd08f3f3295f5fbf9a55c1e7b1f295f550cd
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-02 14:10:06 +0200

    First exercise
    Printing in html array and object

--- Predavanje 12/script.js
+++ Predavanje 12/script.js
@@ -1,1 +1,28 @@
-console.log("test 123");
+// console.log("test 123");
+
+document.addEventListener("DOMContentLoaded", function () {
+	let cities = [
+		{
+			name: "Beograd",
+			temperature: -2,
+			weather: "snowy",
+		},
+		{
+			name: "Novi sad",
+			temperature: 5,
+			weather: "sunny",
+		},
+	];
+
+	for (let i = 0; i < cities.length; i++) {
+		const cityObject = cities[i];
+		let weatherDiv = document.getElementById("weathers");
+		let cityHTML = "";
+
+		for (let key in cityObject) {
+			cityHTML += `<p>${key}: ${cityObject[key]}</p>`;
+		}
+
+		weatherDiv.innerHTML += cityHTML;
+	}
+});
