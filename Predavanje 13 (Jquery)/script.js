2a3c2750a7bef8838659aeab859f3a6a04a4b5e1
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-05 18:43:40 +0200

    A small introduction into jquery

--- Predavanje 13 (Jquery)/script.js
+++ Predavanje 13 (Jquery)/script.js
@@ -1,1 +1,16 @@
-console.log("test 123");
+// console.log("test 123");
+
+// $(document).ready(function () {
+// 	alert("Hello, World! We are using jQuery from local machine");   ----> Testing
+// });
+
+// Razlike i poredjenja jquery & javascript :
+// document.getElementById() in javascript == $(element, element...).ready(function(){}) in jquery
+/* 
+    I na ovakav nacin : $(document).ready(function () {}); se pise sve u jquery
+    A kod se izvrsava tek kada se sve ocita iz HTML-a
+*/
+$(document).ready(function () {
+
+    
+});
