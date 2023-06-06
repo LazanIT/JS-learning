799e07f8d8be8545868e12983fa0bd38fbcc4b9d
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-06 15:25:05 +0200

    First part of home work

---
+++ Predavanje 13 (Jquery)/Domaci/domaci.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$("#additionNumbers").click(function () {
+		let numberOne = parseFloat($("#numberOne").val());
+		let numberTwo = parseFloat($("#numberTwo").val());
+		let addition = numberOne + numberTwo;
+		alert(addition);
+	});
+    $("#")
+});
