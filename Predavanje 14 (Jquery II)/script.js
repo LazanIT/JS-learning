9c6321184c87546ca8281d9c5c0f18154d7bd0c7
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-08 22:09:52 +0200

    Login form exercise part 2
    Testing buttons in JavaScript

--- Predavanje 14 (Jquery II)/script.js
+++ Predavanje 14 (Jquery II)/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
 	// console.log("123");
-    
+
+	let username = "admin";
+	let password = "1234";
+
+	const btnLogin = $("#btnLogin");
+
+	btnLogin.click(function () {
+		console.log("test 123");
+	});
 });
