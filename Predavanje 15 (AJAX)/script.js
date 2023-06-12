5b0fd3c4a0a41026c8fb3f8eccea83e26e2c6fc4
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-12 23:51:32 +0200

    Modified the fifteen lecture

--- Predavanje 15 (AJAX)/script.js
+++ Predavanje 15 (AJAX)/script.js
@@ -1,4 +1,12 @@
 // For loading html ready file
+
 $(document).ready(function () {
-    
+	// Koriscenje AJAX-a :
+	$.ajax({
+		url: "https://reqres.in/api/users?page=2",
+		method: "GET",
+		success: function (informations) {
+			console.log(informations);
+		},
+	});
 });
