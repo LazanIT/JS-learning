9f0a959f957ec491e99ee49bf6676320e267fdd0
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-13 15:22:10 +0200

    First exercise

--- Predavanje 15 (AJAX)/script.js
+++ Predavanje 15 (AJAX)/script.js
@@ -10,6 +10,20 @@ $(document).ready(function () {
 			// Ispisivanje korisnika ili odredjenih zahteva u vezi korisnika
 			console.log(informations["data"][1]["email"]); // Pomocu ove linije koda mi smo ispisali mail od prvog korisnika
 			console.log(informations["data"]); // A pomocu ove linije ispisali smo korisnike iz array-a korisnika
+
+			for (let info in informations["data"]) {
+				console.log(informations["data"][info]);
+				const paragraph = $("#paragraph");
+				paragraph.append(informations["data"][info]["email"]);
+			}
+		},
+	});
+	// Exercise one
+	$.ajax({
+		url: "https://reqres.in/api/users/2",
+		method: "GET",
+		success: function (singleInformation) {
+			console.log(singleInformation["data"]);
 		},
 	});
 });
