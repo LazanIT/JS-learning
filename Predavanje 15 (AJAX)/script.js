fbc80baf2dbb105e74c07ab03857403af57db8c3
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-13 14:25:45 +0200

    Update Ajax
    Printing users from API array, and strict data about user

--- Predavanje 15 (AJAX)/script.js
+++ Predavanje 15 (AJAX)/script.js
@@ -6,7 +6,10 @@ $(document).ready(function () {
 		url: "https://reqres.in/api/users?page=2",
 		method: "GET",
 		success: function (informations) {
-			console.log(informations);
+			console.log(informations["total_pages"]);
+			// Ispisivanje korisnika ili odredjenih zahteva u vezi korisnika
+			console.log(informations["data"][1]["email"]); // Pomocu ove linije koda mi smo ispisali mail od prvog korisnika
+			console.log(informations["data"]); // A pomocu ove linije ispisali smo korisnike iz array-a korisnika
 		},
 	});
 });
