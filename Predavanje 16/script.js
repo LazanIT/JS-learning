64e173c5e94402c71f9e0e3370c26f8fe0bddf42
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-21 21:59:21 +0200

    First exercise finished
    Called weather api for first time

--- Predavanje 16/script.js
+++ Predavanje 16/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-	console.log("123");
+	$.ajax({
+		method: "GET",
+		url: "http://api.weatherapi.com/v1/current.json?key=410dcd3850ec4b00a26194551232106 &q=Belgrade&aqi=yes",
+		success: function (response) {
+			console.log(response);
+		},
+	});
 });
