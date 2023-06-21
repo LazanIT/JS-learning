bb82823a2050863920289f28ca6de6d778333906
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-21 22:03:50 +0200

    Update
    Little explain about data and url

--- Predavanje 16/script.js
+++ Predavanje 16/script.js
@@ -7,3 +7,10 @@ $(document).ready(function () {
 		},
 	});
 });
+// Takodje url se moze pisati i na ovaj nacin :
+// data :{
+//    key: "410dcd3850ec4b00a26194551232106",
+//    q: "Belgrade",
+//    aqi: "yes"
+//}
+// I on ce nama napraviti adresu kao sto je u URL
