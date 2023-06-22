de31f88c51be665d372995e95b61821c15e9c20e
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-22 14:56:18 +0200

    Finished exercise 2
    Forecast days

--- Predavanje 16/script.js
+++ Predavanje 16/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	// First exercise
 	const city = $("#City");
 	$(city).change(function () {
 		let cityValue = $(this).val();
@@ -11,6 +12,26 @@ $(document).ready(function () {
 				key: "410dcd3850ec4b00a26194551232106",
 				q: cityValue,
 				aqi: "yes",
+			},
+			success: function (response) {
+				console.log(response);
+			},
+		});
+	});
+	// Second exercise
+	const btnWeather = $("#btnWeather");
+	$(btnWeather).click(function () {
+		let cityValue = $(city).val();
+		// console.log(cityValue);
+		$.ajax({
+			method: "GET",
+			url: "http://api.weatherapi.com/v1/forecast.json",
+			data: {
+				key: "410dcd3850ec4b00a26194551232106",
+				q: cityValue,
+				aqi: "yes",
+				alerts: "yes",
+				days: "4",
 			},
 			success: function (response) {
 				console.log(response);
