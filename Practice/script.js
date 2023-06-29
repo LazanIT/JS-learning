59afab6412e7ac52aa31a54be474ccf63ed42a11
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-06-29 16:02:17 +0200

    Explain script

--- Practice/script.js
+++ Practice/script.js
@@ -1,16 +1,23 @@
+// Definisanje varijabli za boje
 const ColorOne = "#026873";
 const ColorTwo = "#F29484";
 
+// Uzimanje ID elemenata iz html-a
 let containerElement = document.getElementById("main-container");
 let button = document.getElementById("btn");
 
+// Na dugme smo dodali eventListener click, koji pokrece funkciju changeBackground
 button.addEventListener("click", changeBackground);
 
+// Definisali smo brojac koji ce nam sluziti za bojenje pozadinje
 let counter = 0;
 
+// Funckcija za menjanje boje
 function changeBackground(param) {
+	// Na samom pocetku povecavamo brojac za 1, kako bi postavio main boju
 	counter++;
 
+	// Ako je brojac jednako jedan menjamo boju u boju dva i sledecim klikom u boju jedan
 	if (counter % 2 == 1) {
 		containerElement.style.backgroundColor = ColorOne;
 	} else {
