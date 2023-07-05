5dacd60d74d87aa70615600a1951b5d0eb4dd703
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-07-05 15:59:14 +0200

    Window, document, console

---
+++ Inflation calculator/practice.js
@@ -0,0 +1,11 @@
+// Nacini ispisivanja text-a
+// 1: Ispis iz konzole:
+console.log("Pozdrav svima");
+// A: Takodje postoji i error
+console.error("Pozdrav iz konzole");
+// B: Postoji i warn
+console.warn("Pozdrav iz konzole");
+// 2: Ispis na stranici
+document.write("Pozdrav sa stranice");
+// 3: Alert ispis
+window.alert("Pozdrav iz Popup-a"); // Radi na principu tako sto ispise prvo alert a tek onda load celu stranicu
