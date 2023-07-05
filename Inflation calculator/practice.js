540839213532abb3aedb5de7bda68d8ba273ca6b
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-07-05 16:59:49 +0200

    Update
    Document manipulation with HTML

--- Inflation calculator/practice.js
+++ Inflation calculator/practice.js
@@ -8,11 +8,24 @@ console.warn("Pozdrav iz konzole");
 // 2: Ispis na stranici
 document.write("Pozdrav sa stranice");
 // 3: Alert ispis
-window.alert("Pozdrav iz Popup-a"); // Radi na principu tako sto ispise prvo alert a tek onda load celu stranicu
-window.open(); // Na refresh dobijamo novi tab
+// window.alert("Pozdrav iz Popup-a"); // Radi na principu tako sto ispise prvo alert a tek onda load celu stranicu
+// window.open(); // Na refresh dobijamo novi tab
 // Pored toga sto mozemo ispisivati neki tekst na vise nacina, mozemo raditi i matematicke operacije :
 console.log(5 + 5);
 // A takodje i proveravati da li je vrednost jednaka drugoj vrednosti :
 console.log(5 === 5); // True / False
 
 // Najbitnije od svega toga nam je DOCUMENT :
+
+// Ispis Text-a u HTML dokument
+const messageDiv = document.getElementById("msg");
+messageDiv.innerText = "Pozdrav";
+
+/* Komentar u vise redova
+    ..
+    ..
+    ..
+*/
+
+// Deklarisanje  varijabli :
+
