e4e15f261d8e32724c2754c7a0bbe5e1a1463fa3
Aleksandar Lazanski <117667390+LazanIT@users.noreply.github.com>
2023-07-30 20:02:10 +0200

    Explained
    Value and innerText or innerHTML

--- Inflation calculator/script.js
+++ Inflation calculator/script.js
@@ -3,5 +3,5 @@ const inflationRate = document.querySelector("#inflationRate");
 function inflationCalculator(){
     // console.log("RADI");
 
-    console.log(inflationRate.value);
+    console.log(inflationRate.value); // Za DIV smo imali .innerText a posto je ovo input imamo value
 }
