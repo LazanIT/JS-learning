f4cf5cf4588805a6050e5dab35dc10a7a68b2621
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-01 16:25:38 +0200

    Update

--- Inflation calculator/script.js
+++ Inflation calculator/script.js
@@ -1,16 +1,15 @@
 let inflationRate = document.querySelector("#inflationRate");
 let money = document.querySelector("#money");
+let years = document.querySelector("#years");
 
 function inflationCalculator() {
 	inflationRate = parseFloat(inflationRate.value);
 	money = parseFloat(money.value);
-	// console.log("RADI");
+	years = parseFloat(years.value);
 
 	console.log(typeof inflationRate); // Proveravmao kod su sada tiba da li smo ih sada pretvorili
 
 	// console.log(inflationRate.value);
 	// Za DIV smo imali .innerText a posto je ovo input imamo value
 	// console.log(money.value);
-
-	console.log(inflationRate + money);
 }
