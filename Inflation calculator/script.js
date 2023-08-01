6198f165116eb3af1000d0195e9d164d9dbda0b6
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-01 15:56:47 +0200

    Update Calculator
    Converting string to float or int

--- Inflation calculator/script.js
+++ Inflation calculator/script.js
@@ -1,7 +1,13 @@
-const inflationRate = document.querySelector("#inflationRate");
+let inflationRate = document.querySelector("#inflationRate");
+let money = document.querySelector("#money");
+inflationRate = parseFloat(inflationRate);
+money = parseFloat(money);
+function inflationCalculator() {
+	// console.log("RADI");
 
-function inflationCalculator(){
-    // console.log("RADI");
+	console.log(inflationRate.value);
+	// Za DIV smo imali .innerText a posto je ovo input imamo value
+	console.log(money.value);
 
-    console.log(inflationRate.value); // Za DIV smo imali .innerText a posto je ovo input imamo value
+	console.log(inflationRate.value + money.value);
 }
