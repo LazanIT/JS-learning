385ad77d8a7e17d3da53dcf81589b5efdde0b573
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-01 16:10:50 +0200

    typeof testing

--- Inflation calculator/script.js
+++ Inflation calculator/script.js
@@ -2,14 +2,14 @@ let inflationRate = document.querySelector("#inflationRate");
 let money = document.querySelector("#money");
 
 function inflationCalculator() {
-
-
 	inflationRate = parseFloat(inflationRate.value);
 	money = parseFloat(money.value);
 	// console.log("RADI");
 
+	console.log(typeof inflationRate); // Proveravmao kod su sada tiba da li smo ih sada pretvorili
+
 	// console.log(inflationRate.value);
-    // Za DIV smo imali .innerText a posto je ovo input imamo value
+	// Za DIV smo imali .innerText a posto je ovo input imamo value
 	// console.log(money.value);
 
 	console.log(inflationRate + money);
