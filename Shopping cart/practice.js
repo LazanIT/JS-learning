c13d08ea31f791925989dfe2e96de50a636ba441
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-08 15:24:28 +0200

    Explained how " this " work

--- Shopping cart/practice.js
+++ Shopping cart/practice.js
@@ -1,8 +1,8 @@
 function pokreniMe(element) {
 	let nazivDugmeta = element.innerText;
-
+	let klasaDugmeta = element.className;
 	console.log("Kliknuo si me");
 	let message = document.querySelector("#kliknutoDugme");
-	message.innerText = nazivDugmeta;
+	message.innerText = nazivDugmeta + " - " + klasaDugmeta;
 	console.log(message);
 }
