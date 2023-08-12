e25cfb5db1baa157862a6d34e9ffdf3e0363f581
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-12 15:47:18 +0200

    Update
    main element and price

--- Shopping cart/script.js
+++ Shopping cart/script.js
@@ -1,5 +1,12 @@
 // alert("Js radi");
 
 function addToCart(event) {
-	console.log(event.previousElementSibling.value);
+	let input = event.previousElementSibling.value;
+
+	let mainElement = event.closest(".single-item");
+
+	// querySelector - mi ne moramo raditi samo na document, nego mozemo raditi na celom skupu HTML elemenata
+	let price = mainElement.querySelector(".price").innerText; // Pomocu innerText izvlacimo cenu
+
+	console.log(price.value);
 }
