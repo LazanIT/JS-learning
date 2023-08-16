14b5a6218db497b2dcafffe13dda9faff9e8c6f5
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-16 22:22:43 +0200

    Arrow function

--- Drums app/practice.js
+++ Drums app/practice.js
@@ -1,3 +1,26 @@
 // alert("testing javascript");
 
+// Stari nacin
+function clickMe() {
+	let a = 5;
+	let b = 10;
 
+	console.log(a + b);
+}
+
+clickMe();
+// -----------------------------------
+// Novi nacin
+// Arrow function
+// Kod arrow funkcija se koristi const ( Konstante koje se ne menjaju )
+/*
+    Sto znaci da mi funkcije necemo menjati, nego samo sta da funkcija radi unutar sebe 
+*/
+let clickMeNew = () => {
+	let a = 5;
+	let b = 10;
+
+	console.log(a + b);
+};
+
+clickMeNew();
