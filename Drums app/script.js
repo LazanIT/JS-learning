8284a6db0607bf296d7a925fea1b5eacbbeca8fe
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-22 13:42:05 +0200

    Drums app update
    Testing if keyElemnt is valid or not

--- Drums app/script.js
+++ Drums app/script.js
@@ -1,9 +1,14 @@
 window.addEventListener("keydown", (event) => {
 	let code = event.keyCode;
 
-	let keyElement = document.querySelector(`div[data-key="${code}"]`)
+	let keyElement = document.querySelector(`div[data-key="${code}"]`);
 
-	let audio = document.querySelector(`audio[data-key='${code}'`);
-	audio.currentTime = 0;
-	audio.play();
+	if (keyElement != null) {
+		let audio = document.querySelector(`audio[data-key='${code}'`);
+		audio.currentTime = 0;
+		audio.play();
+	} else {
+		alert("Pritisnuli ste taster koji ne postoji ");
+		return;
+	}
 });
