78ae42b89a14bc7c7a7ff14062cfcd900113adf2
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-08-25 13:38:53 +0200

    Image Slider small project
    Taked buttons for left & right, and assigned click eventListener for both buttons

--- Several small projects/script.js
+++ Several small projects/script.js
@@ -1,3 +1,4 @@
+// Responsive menu
 const mobileMenu = () => {
 	let menu = document.querySelector(".header ul");
 	let btnMenu = document.querySelector(".header button");
@@ -5,8 +6,19 @@ const mobileMenu = () => {
 	if (btnMenu.innerHTML === "MENU") {
 		menu.style.display = "block";
 		btnMenu.innerHTML = "CLOSE";
+		menu.style.transition = "0.4s linear";
 	} else {
 		menu.style.display = "none";
 		btnMenu.innerHTML = "MENU";
 	}
 };
+// Image slider
+let rightBtn = document.querySelector("#right-btn");
+let leftBtn = document.querySelector("#left-btn");
+
+rightBtn.addEventListener("click", () => {
+	console.log("right");
+});
+leftBtn.addEventListener("click", () => {
+	console.log("left");
+});
