a559952c1e1bcd1a3627fede62c4c8fb4d76163b
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-09-04 22:29:29 +0200

    Second step of animated text

--- Several small projects ( Animation )/script.js
+++ Several small projects ( Animation )/script.js
@@ -9,3 +9,12 @@ textTag.innerHTML = "";
 for (let i = 0; i < splittedText.length; i++) {
 	textTag.innerHTML += `<span>${splittedText[i]}</span>`;
 }
+
+let k = 0;
+setInterval(() => {
+	let singleSpan = document.querySelectorAll(".section h1 span");
+
+	console.log(singleSpan);
+
+	k++;
+}, 2000);
