18bdd96576e87578b7813d916742d34a6399b029
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-09-06 21:42:32 +0200

    Animated Text Final Touch

--- Several small projects ( Animation )/script.js
+++ Several small projects ( Animation )/script.js
@@ -1,29 +1,34 @@
-let textTag = document.querySelector(".section h1");
+const animateText = () => {
+	let textTag = document.querySelector(".section h1");
 
-let text = textTag.textContent;
+	let text = textTag.textContent;
 
-let splittedText = text.split("");
+	let splittedText = text.split("");
 
-textTag.innerHTML = "";
+	textTag.innerHTML = "";
 
-for (let i = 0; i < splittedText.length; i++) {
-	if (splittedText[i] == " ") {
-		splittedText[i] = "&nbsp;";
+	for (let i = 0; i < splittedText.length; i++) {
+		if (splittedText[i] == " ") {
+			splittedText[i] = "&nbsp;";
+		}
+		textTag.innerHTML += `<span>${splittedText[i]}</span>`;
 	}
-	textTag.innerHTML += `<span>${splittedText[i]}</span>`;
-}
 
-let k = 0;
-let timeInterval = setInterval(() => {
-	let spans = document.querySelectorAll(".section h1 span");
+	let k = 0;
+	let timeInterval = setInterval(() => {
+		let spans = document.querySelectorAll(".section h1 span");
 
-	let singleSpan = spans[k];
+		let singleSpan = spans[k];
 
-	singleSpan.classList = "fadeMove";
+		singleSpan.classList = "fadeMove";
 
-	k++;
+		k++;
 
-	if (k === spans.length) {
-		clearInterval(timeInterval);
-	}
-}, 600);
+		if (k === spans.length) {
+			clearInterval(timeInterval);
+			setTimeout(animateText, 2000);
+		}
+	}, 60);
+};
+
+animateText();
