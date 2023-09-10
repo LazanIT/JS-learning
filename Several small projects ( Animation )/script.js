d77a8742d2acc5336a6e0fad9b334f1733096f38
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-09-10 13:33:56 +0200

    Fixed bug for Prettier

--- Several small projects ( Animation )/script.js
+++ Several small projects ( Animation )/script.js
@@ -1,39 +1,40 @@
 const animateText = () => {
-	let textTag = document.querySelector(".section h1");
+  let textTag = document.querySelector(".section h1");
 
-	let text = textTag.textContent;
+  let text = textTag.textContent;
 
-	let splittedText = text.split("");
+  let splittedText = text.split("");
 
-	textTag.innerHTML = "";
+  textTag.innerHTML = "";
 
-	for (let i = 0; i < splittedText.length; i++) {
-		if (splittedText[i] == " ") {
-			splittedText[i] = "&nbsp;";
-		}
-		textTag.innerHTML += `<span>${splittedText[i]}</span>`;
-	}
+  for (let i = 0; i < splittedText.length; i++) {
+    if (splittedText[i] == " ") {
+      splittedText[i] = "&nbsp;";
+    }
+    textTag.innerHTML += `<span>${splittedText[i]}</span>`;
+  }
 
-	let k = 0;
-	let timeInterval = setInterval(() => {
-		let spans = document.querySelectorAll(".section h1 span");
+  let spans = document.querySelectorAll(".section h1 span"); // Postavljamo van setInterval funkcije radi optimizacije kod-a
+  let k = 0;
+  let timeInterval = setInterval(() => {
+    let singleSpan = spans[k];
 
-		let singleSpan = spans[k];
+    singleSpan.classList = "fadeMove";
 
-		singleSpan.classList = "fadeMove";
+    k++;
 
-		k++;
-
-		if (k === spans.length) {
-			clearInterval(timeInterval);
-			setTimeout(animateText, 2000);
-		}
-	}, 60);
+    if (k === spans.length) {
+      clearInterval(timeInterval);
+      setTimeout(animateText, 2000);
+    }
+  }, 60);
 };
 
 animateText();
 
-
 window.onscroll(() => {
-	
-})
+  let borderLine = document.querySelector(".border-line");
+  console.log(borderLine);
+  if (borderLine == true) {
+  }
+});
