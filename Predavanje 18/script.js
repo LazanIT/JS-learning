173e718425aa62118dcfde9821d8ab381169e582
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-11-01 22:41:25 +0100

    Defined first class

---
+++ Predavanje 18/script.js
@@ -0,0 +1,17 @@
+// Klasa - CLASS
+// Definicija necega :
+/**
+ * class -
+ *  - Visina
+ *  - Tezina
+ *  - Ime
+ *  - Prezime
+ *
+ *
+ * object -
+ *    - Svako od nas :
+ *      - ime : Aleksa
+ *      - prezime : Lazanski
+ */
+
+class Osoba {} // Imena klasa se uvek pisu velikim slovima
