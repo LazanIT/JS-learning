c36a41c28ccb2b6e5e9a567cea76de99ddf21800
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-11-01 22:45:16 +0100

    First example with class

--- Predavanje 18/script.js
+++ Predavanje 18/script.js
@@ -13,5 +13,18 @@
  *      - ime : Aleksa
  *      - prezime : Lazanski
  */
+// Imena klasa se uvek pisu velikim slovima
+class Osoba {
+  ime;
+  prezime;
+  tezina;
+  visina;
+}
 
-class Osoba {} // Imena klasa se uvek pisu velikim slovima
+let Petar = new Osoba();
+Petar.ime = "Petar";
+Petar.prezime = "Petric";
+Petar.tezina = 70;
+Petar.visina = 189;
+
+console.log(Petar);
