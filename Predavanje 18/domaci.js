b5e45ddc7abc4ed35f40b7cc4d675f7bb4392b6c
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-11-02 22:41:36 +0100

    Text for homework

---
+++ Predavanje 18/domaci.js
@@ -0,0 +1,13 @@
+/**
+ * Domaci :
+ *  Napravite klasu " Vozilo "
+ *     - Boja
+ *     - Vrsta (Vrsta moze biti samo : "automobil", "letelica" ili "plovilo")
+ *
+ *  Napravite klasu " Automobil "
+ *     - Marka
+ *     - Model
+ *     - Broj vrata ( Broj vrata moze biti samo 3 ili 5)
+ *     - Gorivo (Dizel, Benzin, Metan)
+ *
+ */
