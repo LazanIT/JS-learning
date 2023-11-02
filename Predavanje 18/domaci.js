a330641baf5e22f6875e539c02e784ea33c31fd5
LazanIT <117667390+LazanIT@users.noreply.github.com>
2023-11-02 22:45:52 +0100

    Bonus text for homework

--- Predavanje 18/domaci.js
+++ Predavanje 18/domaci.js
@@ -10,4 +10,9 @@
  *     - Broj vrata ( Broj vrata moze biti samo 3 ili 5)
  *     - Gorivo (Dizel, Benzin, Metan)
  *
+ *  BONUS DOMACI : Marka moze biti Audi, BMW, Mercedes
+ *  Model moze biti:
+ *      - Ako je AUDI: A4, A6
+ *      - Ako je Mercedes: S-class, SLS, G-class
+ *      - Ako je BMW :  M5, M3, X1
  */
