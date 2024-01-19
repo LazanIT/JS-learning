a8418ec2e9e155a7a77271ed390b489747510691
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-01-19 23:55:05 +0100

    Best way with class object

--- OOP Practice/script.js
+++ OOP Practice/script.js
@@ -1,38 +1,16 @@
-function Osoba(first_name, last_name, phone) {
-  this.first_name = first_name;
-  this.last_name = last_name;
-  this.phone = phone;
+class Osoba {
+  constructor(first_name, last_name, phone) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone = phone;
+  }
 
-  this.getNameAndPhone = function () {
-    console.log(`${first_name} - ${phone}`);
-  };
+  getNameAndPhone() {
+    console.log(`${this.first_name} - ${this.phone}`);
+  }
 }
-
-let osoba1 = new Osoba("Niko", "Nikolic", "+381");
-let osoba2 = new Osoba("Ivan", "Ivanovic", "+387");
+let osoba1 = new Osoba("Nik0", "Nikolic", "321312");
+let osoba2 = new Osoba("Nikaaaa", "Nikaaaalaaa", "3212");
 
 osoba1.getNameAndPhone();
 osoba2.getNameAndPhone();
-
-// Treci nacin
-
-const Osoba5 = {
-  first_name: "Niko",
-  getNameAndPhone: function () {
-    console.log(`${this.first_name} - ${this.phone}`);
-  },
-};
-
-let niko = Object.create(Osoba5);
-niko.phone = "1234421";
-niko.getNameAndPhone();
-
-let ivan = Object.create(Osoba5);
-ivan.phone = "321321321";
-ivan.first_name = "Ivan";
-ivan.getNameAndPhone();
-
-
-// Best way
-
-class
