6c856a4abd606e427d41bb405b20d6a2f196dd77
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-01-19 23:51:11 +0100

    Third type of creating Osoba

--- OOP Practice/script.js
+++ OOP Practice/script.js
@@ -13,3 +13,26 @@ let osoba2 = new Osoba("Ivan", "Ivanovic", "+387");
 
 osoba1.getNameAndPhone();
 osoba2.getNameAndPhone();
+
+// Treci nacin
+
+const Osoba5 = {
+  first_name: "Niko",
+  getNameAndPhone: function () {
+    console.log(`${this.first_name} - ${this.phone}`);
+  },
+};
+
+let niko = Object.create(Osoba5);
+niko.phone = "1234421";
+niko.getNameAndPhone();
+
+let ivan = Object.create(Osoba5);
+ivan.phone = "321321321";
+ivan.first_name = "Ivan";
+ivan.getNameAndPhone();
+
+
+// Best way
+
+class
