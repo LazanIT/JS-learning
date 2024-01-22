f9fdf9080359c5428e1eb040d07ff4fdc64a3299
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-01-23 00:13:23 +0100

    Validation and Errors
    Validation for name and errors, and validaton for email and errors

--- OOP Practice/validator.js
+++ OOP Practice/validator.js
@@ -16,5 +16,55 @@ class Validator {
 
   inputListener() {
     let inputSelector = this.elementsConfig;
+
+    for (let field in inputSelector) {
+      let selector = `input[name=${field}]`;
+      let element = document.querySelector(selector);
+
+      element.addEventListener("input", this.validate.bind(this));
+    }
+  }
+
+  validate(e) {
+    let elementFields = this.elementsConfig;
+
+    let field = e.target;
+
+    let fieldName = field.getAttribute("name");
+
+    let fieldValue = field.value;
+
+    this.errors[fieldName] = [];
+
+    if (elementFields[fieldName].required) {
+      if (fieldValue === "") {
+        this.errors[fieldName].push("Polje je prazno");
+      }
+    }
+    if (elementFields.email) {
+      if (!this.validateEmail(fieldValue)) {
+        this.errors[fieldName].push("Neispravna email adresa");
+      }
+    }
+
+    if (
+      fieldValue.length < elementFields[fieldName].minlength ||
+      fieldValue.length > elementFields[fieldName].maxlength
+    ) {
+      this.errors[fieldName].push(
+        `Polje mora imati minimalno ${elementFields[fieldName].minlength} i maksimalno ${elementFields[fieldName].maxlength}`
+      );
+    }
+  }
+
+  validateEmail(email) {
+    if (
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+        myForm.emailAddr.value
+      )
+    ) {
+      return true;
+    }
+    return false;
   }
 }
